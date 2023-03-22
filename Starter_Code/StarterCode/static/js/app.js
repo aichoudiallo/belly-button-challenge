aa1c3521461d0879665fd471636ed10699f25cbf
aichoudiallo <119548509+aichoudiallo@users.noreply.github.com>
2023-03-22 19:27:40 -0400

    adding charts

--- Starter_Code/StarterCode/static/js/app.js
+++ Starter_Code/StarterCode/static/js/app.js
@@ -66,9 +66,61 @@ function charts(sample) {
 
         Plotly.newPlot('bar', barData);
 
-        //bubble
+        //bubble chart
+        let bubbleChart = {
+            x: otu_ids,
+            y: sample_values,
+            mode: 'markers',
+            marker: {
+                size: sample_values,
+                color: otu_ids,
+                colorscale: 'Earth'
+            },
+            text: otu_labels
+        };
 
-        //gauge
+        let bubbleLayout = {
+            title: 'Bacteria Cultures Per Sample',
+            xaxis: { title: 'OTU ID' },
+            hovermode: 'closest',
+            showlegend: false
+        };
+
+        let bubbleData = [bubbleChart];
+        Plotly.newPlot('bubble', bubbleData, bubbleLayout);
+
+
+        //gauge chart
+
+        let gaugeData = [
+            {
+                domain: { x: [0, 1], y: [0, 1] },
+                value: wfreq,
+                title: { text: "Scrubs per Week" },
+                type: "indicator",
+                mode: "gauge+number",
+                gauge: {
+                    axis: { range: [null, 9] },
+                    bar: { color: "darkblue" },
+                    steps: [
+                        { range: [0, 1], color: "rgba(255, 255, 255, 0.5)" },
+                        { range: [1, 2], color: "rgba(232, 226, 202, 0.5)" },
+                        { range: [2, 3], color: "rgba(210, 206, 145, 0.5)" },
+                        { range: [3, 4], color: "rgba(202, 209, 95, 0.5)" },
+                        { range: [4, 5], color: "rgba(170, 202, 42, 0.5)" },
+                        { range: [5, 6], color: "rgba(110, 154, 22, 0.5)" },
+                        { range: [6, 7], color: "rgba(14, 127, 0, 0.5)" },
+                        { range: [7, 8], color: "rgba(0, 102, 0, 0.5)" },
+                        { range: [8, 9], color: "rgba(0, 62, 0, 0.5)" }
+                    ]
+                }
+            }
+        ];
+
+        let gaugeLayout = { width: 600, height: 500, margin: { t: 0, b: 0 } };
+        Plotly.newPlot('gauge', gaugeData, gaugeLayout);
+
+
 
     })
 }
